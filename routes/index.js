0011252b0edd9a424c217bd3598d93210dd8fdef
oleg-grigorijan <oleg.grigorijan@gmail.com>
2021-04-03 13:38:13 +0300

    Header is added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('index', {
-        title: 'Express',
+        title: 'ToGO',
         tasks: [
             {
                 id: 'id1',
